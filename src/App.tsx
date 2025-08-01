b292a0ea7c7a28a936941f5d769024e7f6014e2e
Patrick Chan <patrick.chan@staygold.se>
2025-08-01 11:39:51 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Avatar from "./components/avatar";
+
+export default function () {
+  return (
+    <main>
+      <Avatar />
+    </main>
+  );
+}
---
+++ src/components/avatar/index.tsx
@@ -0,0 +1,11 @@
+import avatar from "../../assets/avatar.jpg";
+
+export default function () {
+  return (
+    <img
+      src={avatar}
+      alt="Portrait of Patrick Chan"
+      className="aspect-square rounded-full size-24"
+    />
+  );
+}
