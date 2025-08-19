8ad9a4f554c06ab0cbe815265943956df3b5c0cf
Patrick Chan <patrick.chan@staygold.se>
2025-08-19 21:06:42 +0200

    Wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      components: "/src/components",
+      icons: "/src/icons",
+      assets: "/src/assets",
+    },
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
 });
